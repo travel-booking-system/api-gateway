60d0eb91f4ec3065d2766e51aa329e0ca7a7da89
Raid Abdallah <111711618+radx007@users.noreply.github.com>
2026-02-14 02:32:01 +0100

    feat(gateway): setup routing and service url configuration using Java Records (#2)
    Co-authored-by: Raid Abdallah <111711618+RaDx8@users.noreply.github.com>

--- src/main/java/com/travel/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/travel/api_gateway/ApiGatewayApplication.java
@@ -1,9 +1,12 @@
 package com.travel.api_gateway;
 
+import com.travel.api_gateway.config.ServiceUrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(ServiceUrlConfig.class)
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
